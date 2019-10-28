18016a685192e274dfa87c7e3a463c6950160b8a
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2019-10-28 17:43:06 +0900

    update

--- main_test.go
+++ main_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/hirokazumiyaji/actions-mysql-sample/database"
+	"github.com/schemalex/schemalex"
+	"github.com/schemalex/schemalex/diff"
 )
 
 func TestMain(m *testing.M) {
@@ -20,10 +24,59 @@ func TestMain(m *testing.M) {
 		log.Fatalf("failed to ping: %v", err)
 	}
 
-	_, err = os.Stat("./docker/mysql/sql/initialize.sql")
-	if err != nil {
-		log.Printf("failed to stat: %v\n", err)
+	type query struct {
+		stmt string
+		args []interface{}
 	}
+
+	from, err := schemalex.NewSchemaSource(
+		"mysql://%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local",
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"),
+	)
+	if err != nil {
+		log.Fatalf("failed to new schema source: %v", err)
+	}
+	to, err := shcemalex.NewSchemaSource("./docker/mysql/sql/initialize.sql")
+	if err != nil {
+		log.Fatalf("failed to new schema source: %v", err)
+	}
+
+	stmts := &bytes.Buffer{}
+	p := schemalex.New()
+	err = diff.Strings(stmts, from, to, diff.WithTransaction(true), diff.WithParser(p))
+	if err != nil {
+		return err
+	}
+
+	qs := make([]*query, 0)
+	for _, stmt := range strings.Split(stms, ";") {
+		stmt = strings.TrimSpace(stmt)
+		if len(stmt) == 0 {
+			continue
+		}
+		qs = append(
+			qs,
+			&query{
+				stmt: stmt[0],
+				args: stmt[1:],
+			},
+		)
+	}
+
+	tx := conn.Begin()
+	for _, q := range qs {
+		_, err := tx.Exec(q.stmt, q.args...)
+		if err != nil {
+			log.Printf("failed to exec sql: %v", err)
+			tx.Rollback()
+			return
+		}
+	}
+	tx.Commit()
 
 	os.Exit(m.Run())
 }
